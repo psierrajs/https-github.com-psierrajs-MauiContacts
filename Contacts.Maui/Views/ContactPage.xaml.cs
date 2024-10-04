eee0117dcc95ece5d07218448f77e13a9f91aec6
psierrajs <psierra.js@gmail.com>
2024-10-04 19:06:54 +0200

    Agregar archivos de proyecto.

---
+++ Contacts.Maui/Views/ContactPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Contacts.Maui.Views;
+
+public partial class ContactPage : ContentPage
+{
+	public ContactPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Contacts.Maui/Views/EditContactPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Contacts.Maui.Views;
+
+public partial class EditContact : ContentPage
+{
+	public EditContact()
+	{
+		InitializeComponent();
+	}
+}
