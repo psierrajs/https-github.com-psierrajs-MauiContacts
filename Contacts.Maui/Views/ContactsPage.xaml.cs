643b5322facd2e5dc6ae9a84b43573411e158e17
psierrajs <psierra.js@gmail.com>
2024-10-05 19:38:08 +0200

    @1:14:28

--- Contacts.Maui/Views/ContactsPage.xaml.cs
+++ Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -31,14 +31,17 @@ public partial class ContactsPage : ContentPage
         public string? Email { get; set; }
     }
 
-    private void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    private async void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
-        DisplayAlert("test", "test", "OK");
-        listContacts.SelectedItem = null;
+        if (listContacts.SelectedItem != null)
+        {
+            //logic
+            await Shell.Current.GoToAsync(nameof(EditContactPage));
+        }
     }
 
     private void listContacts_ItemTapped(object sender, ItemTappedEventArgs e)
     {
-
+        listContacts.SelectedItem = null;
     }
 }
