fc6c95e460d069276cdcf0aac0549d6056f98430
psierrajs <psierra.js@gmail.com>
2024-10-05 19:12:58 +0200

    @1.03:26 Cover ListView basics

--- Contacts.Maui/Views/ContactsPage.xaml.cs
+++ Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -3,18 +3,33 @@ namespace Contacts.Maui.Views;
 
 public partial class ContactsPage : ContentPage
 {
-	public ContactsPage()
-	{
+    public ContactsPage()
+    {
         InitializeComponent();
-	}
 
-    private void btnEditContact_Clicked(object sender, EventArgs e)
-    {
-        Shell.Current.GoToAsync(nameof(EditContactPage));
+        List<Contact> contacts = new List<Contact>()
+        {
+        new Contact { Name = "John Doe", Email = "JohnDoe@gmail.com" },
+        new Contact { Name = "Jane Doe", Email = "JaneDoe@yahoo.com" },
+        new Contact { Name = "Tom Hanks", Email = "TomHanks@hollywood.com" },
+        new Contact { Name = "Frank Liu", Email = "FrankLiu@gmail.com" },
+        new Contact { Name = "Emily Clark", Email = "EmilyC@gmail.com" },
+        new Contact { Name = "Michael Smith", Email = "MichaelS@outlook.com" },
+        new Contact { Name = "Sophia Williams", Email = "SophiaW@yahoo.com" },
+        new Contact { Name = "David Johnson", Email = "DavidJ@gmail.com" },
+        new Contact { Name = "Linda Brown", Email = "LindaB@gmail.com" },
+        new Contact { Name = "James White", Email = "JamesWhite@company.com" }
+        };
+
+
+        listContacts.ItemsSource = contacts;
     }
 
-    private void btnAddContact_Clicked(object sender, EventArgs e)
+    public class Contact
     {
-        Shell.Current.GoToAsync(nameof(AddContactPage));
+        public string Name { get; set; }
+        public string Email { get; set; }
     }
+
+
 }
