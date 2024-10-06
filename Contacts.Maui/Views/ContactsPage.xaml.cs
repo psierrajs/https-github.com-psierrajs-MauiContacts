12a2083b8e3c27df515835d8bf9b735615927db6
psierrajs <psierra.js@gmail.com>
2024-10-06 15:57:48 +0200

    @2:12.35

--- Contacts.Maui/Views/ContactsPage.xaml.cs
+++ Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -1,4 +1,5 @@
 using Contacts.Maui.Models;
+using System.Collections.ObjectModel;
 using Contact = Contacts.Maui.Models.Contact;
 
 namespace Contacts.Maui.Views;
@@ -9,13 +10,18 @@ public partial class ContactsPage : ContentPage
     {
         InitializeComponent();
 
-        List<Contact> contacts = ContactRepository.GetContacts();
+        
+    }
 
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
 
+        var contacts = new ObservableCollection<Contact>(ContactRepository.GetContacts());
         listContacts.ItemsSource = contacts;
     }
 
-    
+
 
     private async void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
